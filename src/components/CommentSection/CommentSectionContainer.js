7de8190b45cc77c1302266a8e2cd13efc52b6304
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-23 22:34:59 -0400

    Fixed CommentsSection

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,14 +3,15 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "../../dummy-data";
 
 const CommentSection = props => {
   // Add state for the comments
-  const [comment] = useState("");
+  const [comment, setComment] = useState(dummyData);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      {Comment.props.comment.map(element => {
+      {comment.map(element => {
         return <Comment comment={element} />
       })};
       <CommentInput />
